03267d81c1ef0fd6de19186160058be014f5747e
wangmz <w2639764449@hotmail.com>
2023-01-15 16:39:34 +0800

    New Commit from wangmz

--- .p10k.zsh
+++ .p10k.zsh
@@ -79,7 +79,6 @@
     kubecontext             # current kubernetes context (https://kubernetes.io/)
     terraform               # terraform workspace (https://www.terraform.io)
     # terraform_version     # terraform version (https://www.terraform.io)
-    aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
     aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
     azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)
@@ -113,6 +112,7 @@
     # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
+    aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
   )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
